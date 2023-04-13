c8aee8baa4c1dec108712e425da0e26e540e0b15
AleixCarles <aleixcarles@iesebre.com>
2023-04-13 17:19:33 +0200

    Video 105 Loading States Skeleton test al initial loading i Pull To Refresh finalitzat

---
+++ src/views/Videos.vue
@@ -0,0 +1,46 @@
+<template>
+    <ion-list>
+        <ion-list-header>
+            Recent Conversations
+        </ion-list-header>
+
+        <ion-item v-for="video in videos" :key="video.message">
+            <ion-avatar slot="start">
+                <img src="https://via.placeholder.com/80">
+            </ion-avatar>
+            <ion-label>
+                <h2>{{ video.title }}</h2>
+                <p>{{ video.description}}</p>
+            </ion-label>
+        </ion-item>
+
+    </ion-list>
+</template>
+
+<script>
+import {IonList, IonItem, IonListHeader, IonAvatar, IonLabel} from "@ionic/vue";
+import casteaching from "@acacha/casteaching";
+const api = casteaching({baseUrl:'http://casteaching.test/api'})
+export default {
+    name: "Videos",
+    components: {
+        IonList,
+        IonItem,
+        IonListHeader,
+        IonAvatar,
+        IonLabel
+    },
+    data(){
+        return{
+            videos: []
+        }
+    },
+    async created() {
+        this.videos = await api.videos()
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
